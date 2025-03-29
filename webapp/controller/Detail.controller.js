3c9e621bb323ed3ddcbab49188198c8a851261b4
TUSHAR SHUKLA <tusharshukla0907@gmail.com>
2025-03-29 07:06:22 +0000

    correction

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,8 +1,11 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/odata/v2/ODataModel"
+    "sap/ui/model/odata/v2/ODataModel",
+    "sap/ui/model/json/JSONModel"
+
+
 ],
-function (Controller,ODataModel) {
+function (Controller,ODataModel,JSONModel) {
     "use strict";
 
     return Controller.extend("com.tushar.project1.controller.Detail", {
@@ -18,12 +21,12 @@ function (Controller,ODataModel) {
             this.sPID = sPID;
 
             const oModel = that.getOwnerComponent().getModel();
-            oModel.read("A_SalesOrder('" + sPID + "')/to_Item", {
+            oModel.read("/A_SalesOrder('"+sPID+"')/to_Item/", {
                 success : function(oSALESDETAIL, response) {
                    console.log('SALES DATA IS', oSALESDETAIL)
-                   let oJSONMOdel1 = new JSONModel()
-                   oJSONMOdel1.setData(oPRODUCTDETAIL)
-                   that.getView().setModel(oJSONMOdel1, "S")    
+                   let oJSONMOdel = new JSONModel()
+                   oJSONMOdel.setData(oSALESDETAIL)
+                   that.getView().setModel(oJSONMOdel,"S")    
                 },
                 
                 error: function(oError) {
